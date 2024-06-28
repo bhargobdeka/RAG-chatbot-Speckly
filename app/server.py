8db35408101ca4bc3dbffef1ac8c56c7df5f99fb
bhargobdeka <69599126+bhargob-deka@users.noreply.github.com>
2024-06-28 16:19:55 -0400

    update v1

--- app/server.py
+++ app/server.py
@@ -36,18 +36,18 @@ os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
 
 # WebSearch tools
-os.environ['SERPAPI_API_KEY'] = os.getenv('SERPAPI_API_KEY')
-os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
-os.environ['GOOGLE_CSE_ID'] = os.getenv('GOOGLE_CSE_ID')
-os.environ['TAVILY_API_KEY'] = os.getenv('TAVILY_API_KEY')
+# os.environ['SERPAPI_API_KEY'] = os.getenv('SERPAPI_API_KEY')
+# os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
+# os.environ['GOOGLE_CSE_ID'] = os.getenv('GOOGLE_CSE_ID')
+# os.environ['TAVILY_API_KEY'] = os.getenv('TAVILY_API_KEY')
 
 # Langsmith Tracing
-os.environ['LANGCHAIN_TRACING_V2'] = os.getenv('LANGCHAIN_TRACING_V2')
-os.environ['LANGCHAIN_ENDPOINT'] = os.getenv('LANGCHAIN_ENDPOINT')
-os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
+# os.environ['LANGCHAIN_TRACING_V2'] = os.getenv('LANGCHAIN_TRACING_V2')
+# os.environ['LANGCHAIN_ENDPOINT'] = os.getenv('LANGCHAIN_ENDPOINT')
+# os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
 
 # Fire Crawl API
-os.environ['FIRE_API_KEY']=os.getenv('FIRE_API_KEY')
+# os.environ['FIRE_API_KEY']=os.getenv('FIRE_API_KEY')
 
 ## Create Retriever
 
